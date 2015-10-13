443a32caad8866c68fee397fc98510ce27904c85
SynedraAcus <alexeymorozov1991@gmail.com>
2015-10-13 13:11:20 +0800

    Code push

---
+++ reductor.py
@@ -0,0 +1,36 @@
+__author__ = 'morozov'
+
+class SequenceSet(object):
+    '''
+    Container class for sequences.
+    '''
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def append(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+class DistanceMatrix(object):
+    '''
+    Distance matrix class. Contains a list of IDs and a distance matrix for the same IDs
+    '''
+    def __init__(self):
+        pass
+
+    def dj(self, final_count):
+        pass
+
+    def append(self, item):
+        pass
+
+    def remove(self, item):
+        pass
---
+++ sampler.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+__author__ = 'morozov'
+
+import argparse
+from Bio import SeqIO
+import reductor
+import sys
+
+arg_parser = argparse.ArgumentParser(description='Reduce sequence dataset using Distant Joining approach')
+arg_parser.add_argument('-f', type=str, help='Fasta file with sequence set to be reduced')
+arg_parser.add_argument('-d', type=str, help='Distance matrix file in PHYLIP square format')
+arg_parser.add_argument('-n', type=str, help='Number (int) or percentage (float) of sequences to be sampled\nDefault is 0.2',\
+                        default='0.2')
+args = arg_parser.parse_args()
+if not(args.f or args.d):
+    sys.stderr.write('Either -f or -d option should be used')
+
+#  Insert matrix/seq parsing here so we can calculate uniform number
+
+if '.' in args.n:
+    final_number = float(args.n)
+else:
+    final_number = int(args.n)
